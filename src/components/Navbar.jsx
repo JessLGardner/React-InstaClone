3a8fd6ad62037c72229f8b27e54c39b56b98cf9d
Jess Gardner <inanna1130@gmail.com>
2017-08-09 07:03:58 -0400

    finished Navbar styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,30 +8,43 @@ const NavStyle = styled.div`
   align-items: center;
   justify-content: space-between;
   height: 70px;
-  background-color: #FFFFF1;
-  padding: 30px;
+  background-color: #FFFFFF;
+  padding: 10px 40px;
   border-bottom: 1px solid rgba(0,0,0,.2);
+    p{
+    font-family: cursive;
+    font-size: 30px;
+    }
 `;
 
 const SearchStyle = styled.div`
+  background: #FAFAFA;
+  border: 1px solid rgba(0,0,0,.2);
+  padding: 7px;
+  color: #999;
+  width: 20%;
+  input{
+    border: none;
+    font-weight: 300;
+    background: transparent;
+    text-align: center;
+  }
 `;
 
+
 const IconStyle = styled.div`
-  width: 25%
+  width: 250px;
   display: flex;
   justify-content: space-around;
   `;
-
 
 class Navbar extends Component{
   render(){
     return (
       <NavStyle>
 
-        <div>
-          <h3><FaInstagram /> | Instaclone</h3>
-        </div>
-
+          <p><FaInstagram /> | Instaclone</p>
+      
         <SearchStyle>
           <FaSearch/><input type="text"/>
         </SearchStyle>
@@ -48,68 +61,4 @@ class Navbar extends Component{
 }
 
 
-export default Navbar
-
-
-
-// const Nav = styled.div`
-//   background-color: white;
-//   display: flex;
-//   justify-content: space-between;
-//   align-items: center;
-//   padding: 26px 5vw;
-//   width: 90vw;
-//   height: 25px;
-//   border-bottom: 1px solid rgba(0,0,0,.0975);
-
-//   p{
-//     font-size: 1.75rem;
-//     font-family: 'Oleo Script', cursive;
-//     margin: 0;
-//     svg{
-//       margin-bottom: 5px;
-//     }
-//   }
-// `
-
-// const SearchContainer = styled.div`
-//   background: #fafafa;
-//   border: 1px solid #dbdbdb;
-//   width: 25%;
-//   padding: 7px;
-//   border-radius: 3px;
-//   color: #999;
-//   svg{
-//     margin-bottom: 5px;
-//   }
-//   input{
-//     border: none;
-//     font-weight: 300;
-//     background: transparent;
-//     text-align: center;
-//   }
-// `;
-
-// const IconsContainer = styled.div`
-//   width: 25%;
-//   display: flex;
-//   justify-content: space-around;
-// `;
-
-// class Navbar extends Component{
-//   render(){
-//     return (
-//       <Nav>
-//         <p><FaInstagram /> | Instaclone</p>
-//         <SearchContainer>
-//           <FaSearch/><input type="text" placeholder="Search"/>
-//         </SearchContainer>
-//         <IconsContainer>
-//           <FaCompass size={28} />
-//           <FaHeartO size={28} />
-//           <FaUser size={28} />
-//         </IconsContainer>
-//       </Nav>
-//     );
-//   }
-// }
+export default Navbar;
